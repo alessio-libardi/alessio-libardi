303798d2ca97c70816401a1499cbe98be18f9baf
scanace <alessiolibardi@gmail.com>
2020-07-28 15:49:46 +0200

    Removed tests and e2e

---
+++ apps/portfolio/src/app/feature/about/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alessio-libardi-root',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+})
+export class AboutComponent {
+}
---
+++ apps/portfolio/src/app/feature/welcome/welcome.component.ts
@@ -0,0 +1,9 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'alessio-libardi-root',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss'],
+})
+export class WelcomeComponent {
+}
