5876bd2266022134d24b75780ce03effce11eee9
Alessio Libardi <alessio@wonderflow.co>
2021-03-10 21:55:11 +0100

    Created simple minimalistic website

--- apps/portfolio/src/app/feature/welcome/welcome.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, HostListener } from '@angular/core';
-
-@Component({
-  selector: 'alessio-libardi-root',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
-})
-export class WelcomeComponent {}
